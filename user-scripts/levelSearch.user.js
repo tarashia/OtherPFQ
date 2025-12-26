83138fa4899721fff6ef4c4433c30ea311724e45
kareesnc <108560481+tarashia@users.noreply.github.com>
2025-12-26 03:34:59 -0500

    Create levelSearch.user.js

---
+++ user-scripts/levelSearch.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name     PFQ Shelter: Level Search
+// @namespace    https://github.com/tarashia/
+// @author       Mirzam
+// @include      https://pokefarm.com/shelter
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+/* Change these to modify script behavior */
+const minimumLevel = 80;
+const highlightColor = "white";
+const addGlow = true;
+const addBorder = false;
+const glowStacks = 4;
+/* glowStacks controls how big the glow is, if using glow */
+
+
+/* Don't change below here */
+const addClass = "foundHighLevel";
+
+function addCSS() {
+  let cssElem = document.createElement('style');
+  cssElem.type = "text/css";
+  document.getElementsByTagName('head')[0].append(cssElem);
+  cssElem.innerHTML = "."+addClass+" {";
+  if(addGlow) {
+    cssElem.innerHTML += "filter:";
+    for(let i=0; i<glowStacks; i++) {
+      cssElem.innerHTML += "drop-shadow(0 0 3px "+highlightColor+")";
+    }
+    cssElem.innerHTML += ";";
+  }
+  if(addBorder) {
+    cssElem.innerHTML += "border: 4px solid "+highlightColor+";";
+  }
+  cssElem.innerHTML += "}";
+}
+
+function tagPokemon() {
+  const shelterArea = document.getElementById('shelterarea');
+  const pkmn = shelterArea.getElementsByClassName('tooltip_content');
+  const regex = /\(Lv\.(\d+)/;
+  for(let i=0; i<pkmn.length; i++) {
+    let matches = pkmn[i].innerHTML.match(regex);
+    if(matches && matches.length > 1) {
+      let pkmnLvl = parseInt(matches[1]);
+      if(pkmnLvl >= minimumLevel) {
+        pkmn[i].previousSibling.classList.add(addClass);
+      }
+    }
+  }
+}
+
+function observeShelter() {
+  const shelterArea = document.getElementById('shelterarea');
+  if(!shelterArea) {
+    return false;
+  }
+  const observerConfig = { childList: true, characterData: false };
+  const observer = new MutationObserver(function() {
+    tagPokemon();
+  });
+  observer.observe(shelterArea, observerConfig);
+  return observer;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async function(){
+  let observer = false;
+  let attempts = 0;
+  while(observer === false && attempts < 10) {
+    await sleep(500);
+    attempts ++;
+		observer = observeShelter();
+  }
+  addCSS();
+  tagPokemon();
+})();
